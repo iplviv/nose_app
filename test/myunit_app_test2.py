a7f1dfce94f645f32d1ee9bb26293d797ca4060b
Ivan Lesnik <ipLviv@gmail.com>
2015-11-04 16:22:31 +0200

    Fix tests

--- test/myunit_app_test2.py
+++ test/myunit_app_test2.py
@@ -4,9 +4,9 @@ import myunit.app
 class MyUnitTest(unittest.TestCase):
 
   def testInit(self):
-    app = myunit.app.App(100)
-    self.assertEquals(app.var, 100)
+    app = myunit.app.App(300)
+    self.assertEquals(app.var, 302)
 
   def testGetVar(self):
-    app = myunit.app.App(200)
-    self.assertEquals(app.get_var(), 200)
+    app = myunit.app.App(400)
+    self.assertEquals(app.get_var(), 402)
