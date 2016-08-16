4148fe8a35ce34539ba027273ab6906ad0afe37c
Ivan Lesnik <ipLviv@gmail.com>
2016-08-16 16:59:14 +0300

    Spike time (0.0-0.7)

--- test/number_test2.py
+++ test/number_test2.py
@@ -1,14 +1,17 @@
+import time
+
 def one_test():
   assert 1
 
 def two_test():
+  if time.time() / 60 % 2 < 0.7:
+    time.sleep(30)
   assert 2
 
 def three_test():
   assert 1 + 2
 
 def four_test():
-  import time
   if time.time() / 60 % 2 > 1.5:
     result = 15
   else:
